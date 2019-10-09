b212246cfc8862b98c1885f482840637271a4a7a
aaronjones2 <aaron.jones2@unilever.com>
2019-10-09 12:31:25 +0100

    adding !

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Top Fives</h1>
+    <h1>Top Fives!</h1>
     <p>A site commited to displaying all the top fives of all types of 'thing' in the world!</p>
     <h2>Animals</h2>
     <ul>
