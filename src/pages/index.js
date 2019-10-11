19e043c26245034eaaf2fde7bf7d0f604aeda01d
aaronjones2 <aaron.jones2@unilever.com>
2019-10-11 16:13:24 +0100

    dubliners top 5, and fix to Alco drinks

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,8 @@ const IndexPage = () => (
       <Tile title="Bridges" one="Waterloo Bridge" two="Hammersmith Bridge" three="Millenium Bridge" four="Brooklyn Bridge" five="Clifton Suspension Bridge" />
       <Tile title="Breads" one="Olive Bread" two="Garlic Bread" three="Tiger Bread" four="Naan Bread" five="Soft Brown Bap" />
       <Tile title="SkyScrapers" one="Shard" two="Empire Estate Building" three="Gherkin" four="Chrysler Tower" five="One World Trade Center" />
-      <Tile title="Alcoholic drinks" one="Guiness (Stout)" two="Jamesons (Whiskey)" three="Tequila" four="IPA" five="Lager" />
+      <Tile title="Alcoholic drinks" one="Guiness (Stout)" two="Jamesons (Whiskey)" three="Red Wine" four="IPA" five="Tequila" />
+      <Tile title="Songs by The Dubliners" one="Raglan Road" two="The Musical Priest" three="Dirty Old Town" four="School Days Over" five="Skibereen" />
 
     </ul>
 
