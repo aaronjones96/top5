157d0219de162c8ee6d95309b9dfc3a96ca42e16
aaronjones2 <aaron.jones2@unilever.com>
2019-10-14 16:17:11 +0100

    deserts

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,21 @@ import Tile from "../components/tile";
 
 const IndexPage = () => (
   <Layout>
+    
     <SEO title="Home" />
+
     <p>A site commited to displaying all the top fives of all types of 'thing' in the world!</p>
+
     <ul className="grid">
+
       <Tile title="Animals" one="Squirrel" two="Dog" three="Wolf" four="Tiger (White)" five="Seal" />
       <Tile title="Bridges" one="Waterloo Bridge" two="Hammersmith Bridge" three="Millenium Bridge" four="Brooklyn Bridge" five="Clifton Suspension Bridge" />
       <Tile title="Breads" one="Olive Bread" two="Garlic Bread" three="Tiger Bread" four="Naan Bread" five="Soft Brown Bap" />
       <Tile title="SkyScrapers" one="Shard" two="Empire Estate Building" three="Gherkin" four="Chrysler Tower" five="One World Trade Center" />
       <Tile title="Alcoholic drinks" one="Guiness (Stout)" two="Jamesons (Whiskey)" three="Red Wine" four="IPA" five="Tequila" />
-      <Tile title="Songs by The Dubliners" one="Raglan Road" two="The Musical Priest" three="Dirty Old Town" four="School Days Over" five="Skibereen" />
+      <Tile title="Songs by The Dubliners" one="Raglan Road" two="The Musical Priest" three="Dirty Old Town" four="School Days Over" five="McAlpines Fusiliers" />
+      <Tile title="Deserts" one="Banoffee Pie" two="Eton Mess" three="Ice Cream" four="Rice Pudding (with Jam)" five="Angel Delight" />
+
 
     </ul>
 
